13e6a11cac36533637c5aa64455fe6e0461e5016
Kontroll <72424960+KontrollFreek@users.noreply.github.com>
2022-01-14 22:06:57 -0500

    Add files via upload

---
+++ penislist/js/list.js
@@ -0,0 +1,571 @@
+const d = {
+	"list": [
+		/*=================================================================================*/
+		{
+			"vids": [
+				// {
+				// 	"user": "",
+				// 	"link": "",
+				// 	"percent": 0,
+				// 	"hz": ""
+				// },
+
+
+			],
+			"name": "Crucible",
+			"author": "Cukik",
+			"more": "none",
+			"id": 72280502,
+			"pass": "Free To Copy",
+			"percentToQualify": 64,
+			"verificationVid": "https://www.youtube.com/watch?v=_VjRTwDue_c",
+			/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 0
+
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "Cukik",
+					"link": "https://youtu.be/MHJvM0Ies3M",
+					"percent": 100,
+					"hz": "240hz"
+				},
+
+			],
+			"name": "Crack Blizzard",
+			"author": "Iowa",
+			"more": "none",
+			"id": 65588509,
+			"pass": "None",
+			"percentToQualify": 51,
+			"verificationVid": "https://youtu.be/MHJvM0Ies3M",
+			/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 1
+
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "Hanocrux",
+					"link": "https://youtu.be/p2rxEIn2Xmg",
+					"percent": 100,
+					"hz": "60hz"
+				},
+
+
+			],
+			"name": "Water Ice Salt Aye",
+			"author": "Cukik",
+			"more": "none",
+			"id": 72393049,
+			"pass": "Free To Copy",
+			"percentToQualify": 64,
+			"verificationVid": "https://www.youtube.com/watch?v=-KapVecXaXg",
+			/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 2
+
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "Iowa",
+					"link": "https://youtu.be/MHJvM0Ies3M",
+					"percent": 100,
+					"hz": "60hz"
+				},
+
+			],
+			"name": "Hazbin Hotel",
+			"author": "Entropy [hug]",
+			"more": "none",
+			"id": 65286831,
+			"pass": "None",
+			"percentToQualify": 39,
+			"verificationVid": "https://www.youtube.com/watch?v=epWIucwGb8s&t=4s",
+			/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 3
+
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "KaiHuntYT",
+					"link": "https://www.youtube.com/watch?v=26kGAJqRfrE",
+					"percent": 100,
+					"hz": "144hz"
+				},
+				{
+					"user": "hug",
+					"link": "https://www.youtube.com/watch?v=ybWHCv9MlDQ",
+					"percent": 100,
+					"hz": "60hz"
+				},
+				{
+					"user": "Cukik",
+					"link": "https://www.youtube.com/watch?v=aS9zo6qAsfI",
+					"percent": 100,
+					"hz": "240hz"
+				},
+
+			],
+			"name": "Greasy Ass II",
+			"author": "Entropy [Amnesticat]",
+			"more": "none",
+			"id": 65235712,
+			"pass": "None",
+			"percentToQualify": 41,
+			"verificationVid": "https://www.youtube.com/watch?v=oR9jVuiENz0",
+			/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 4
+
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "Cukik",
+					"link": "https://www.youtube.com/watch?v=qMAOI4nVArc",
+					"percent": 100,
+					"hz": "300hz"
+				},
+
+			],
+			"name": "Kelenic",
+			"author": "Entropy [HanoCrux]",
+			"more": "none",
+			"id": 72186814,
+			"pass": "None",
+			"percentToQualify": 51,
+			"verificationVid": "https://www.youtube.com/watch?v=6FQFk5kH2W4&t=11s",
+			/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 5
+
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "Iowa",
+					"link": "https://www.youtube.com/watch?v=w14MJvUL1DU",
+					"percent": 100,
+					"hz": "60hz"
+				},
+				{
+					"user": "Cukik",
+					"link": "https://www.youtube.com/watch?v=iNhG8gwZNNs",
+					"percent": 100,
+					"hz": "300hz"
+				},
+
+			],
+			"name": "BALTIMORE",
+			"author": "Entropy [Amnesticat]",
+			"more": "none",
+			"id": 65423867,
+			"pass": "None",
+			"percentToQualify": 56,
+			"verificationVid": "https://www.youtube.com/watch?v=stP6Se3xTqw",
+			/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 6
+
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "kirbycube",
+					"link": "https://youtu.be/JocghoVCNqg",
+					"percent": 100,
+					"hz": "60hz"
+				},
+				{
+					"user": "Hanocrux",
+					"link": "https://youtu.be/w5xnF_9Ryfk",
+					"percent": 100,
+					"hz": "60hz"
+				},
+
+			],
+			"name": "Burnt Gasoline",
+			"author": "Entropy",
+			"more": "none",
+			"id": 73580766,
+			"pass": "None",
+			"percentToQualify": 51,
+			"verificationVid": "https://youtu.be/7dfb3MvDOrk",
+			/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 7
+
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "Cukik",
+					"link": "https://www.youtube.com/watch?v=AVanSZc56IY",
+					"percent": 100,
+					"hz": "300hz"
+				},
+
+			],
+			"name": "Naite",
+			"author": "Entropy",
+			"more": "none",
+			"id": 72348071,
+			"pass": "None",
+			"percentToQualify": 51,
+			"verificationVid": "https://www.youtube.com/watch?v=8bPzYZpTC_o",
+			/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 8
+
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "Iowa",
+					"link": "https://youtu.be/MHJvM0Ies3M",
+					"percent": 100,
+					"hz": "60hz"
+				},
+
+			],
+			"name": "Erebus",
+			"author": "Entropy",
+			"more": "none",
+			"id": 76173704,
+			"pass": "None",
+			"percentToQualify": 45,
+			"verificationVid": "https://youtu.be/5O_hUXWX6Ak",
+			/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 9
+
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "St3am",
+					"link": "https://www.youtube.com/watch?v=eu83_XaWsvo",
+					"percent": 100,
+					"hz": "90hz"
+				},
+				{
+					"user": "hug",
+					"link": "https://www.youtube.com/watch?v=OO6HG8uuiE8",
+					"percent": 100,
+					"hz": "60hz"
+				},
+				{
+					"user": "Cukik",
+					"link": "https://www.youtube.com/watch?v=jDsoxm_9Ugw",
+					"percent": 100,
+					"hz": "300hz"
+				},
+				{
+					"user": "AdoraH",
+					"link": "https://www.youtube.com/watch?v=vEwCDvnRn-Q",
+					"percent": 100,
+					"hz": "60hz"
+				},
+
+			],
+			"name": "Y",
+			"author": "Entropy",
+			"more": "none",
+			"id": 72348105,
+			"pass": "None",
+			"percentToQualify": 49,
+			"verificationVid": "https://www.youtube.com/watch?v=Er0R5UGsT9k",
+			/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 10
+
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "Amnesticat",
+					"link": "https://www.youtube.com/watch?v=Xmr1yzIstJ8",
+					"percent": 100,
+					"hz": "60hz"
+				},
+
+			],
+			"name": "Final Destination N",
+			"author": "Decoration",
+			"more": "none",
+			"id": 65129338,
+			"pass": "None",
+			"percentToQualify": 49,
+			"verificationVid": "https://www.youtube.com/watch?v=2MgsTYDZcvM",
+			/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 11
+
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "Cukik",
+					"link": "https://www.youtube.com/watch?v=zj6uJLP1b6c",
+					"percent": 100,
+					"hz": "300hz"
+				},
+
+			],
+			"name": "Stage 4 of AIDS",
+			"author": "HanoCrux",
+			"more": "none",
+			"id": 72237408,
+			"pass": "None",
+			"percentToQualify": 50,
+			"verificationVid": "https://www.youtube.com/watch?v=esBYakDE58g",
+			/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 12
+
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "Entropy",
+					"link": "https://youtube.com/shorts/9aGweDsR0_w?feature=share",
+					"percent": 100,
+					"hz": "60hz"
+				},
+				{
+					"user": "Hanocrux",
+					"link": "https://youtu.be/hkuStOO1FF0",
+					"percent": 100,
+					"hz": "60hz"
+				},
+
+			],
+			"name": "Whoppa",
+			"author": "Decoration",
+			"more": "none",
+			"id": 72345348,
+			"pass": "None",
+			"percentToQualify": 50,
+			"verificationVid": "https://www.youtube.com/watch?v=E7GSRC2wOgQ",
+			/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 13
+
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "Cukik",
+					"link": "https://www.youtube.com/watch?v=yh8LEtxT2S8",
+					"percent": 100,
+					"hz": "70hz"
+				},
+
+			],
+			"name": "UFWM Challenge",
+			"author": "HanoCrux",
+			"more": "none",
+			"id": 72166924,
+			"pass": "None",
+			"percentToQualify": 50,
+			"verificationVid": "https://www.youtube.com/watch?v=7ithggVN5pU",
+			/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 14
+
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "HanoCrux",
+					"link": "https://www.youtube.com/watch?v=4hYvWBtN-bY",
+					"percent": 100,
+					"hz": "60hz"
+				},
+
+			],
+			"name": "4 20",
+			"author": "Cukik",
+			"more": "none",
+			"id": 68507101,
+			"pass": "None",
+			"percentToQualify": 50,
+			"verificationVid": "https://www.youtube.com/watch?v=QP43B3jzxFU",
+			/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 15
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "Cukik",
+					"link": "https://www.youtube.com/watch?v=y9R2BYK1ZZg",
+					"percent": 100,
+					"hz": "300hz"
+				},
+				{
+					"user": "KontrollFreek",
+					"link": "https://youtu.be/AJvHUCtcer8",
+					"percent": 100,
+					"hz": "60hz"
+				},
+
+			],
+			"name": "the spider",
+			"author": "Screedican",
+			"more": "none",
+			"id": 72042337,
+			"pass": "None",
+			"percentToQualify": 50,
+			"verificationVid": "https://www.youtube.com/watch?v=n3HHyuKFyFY",
+			/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 20
+
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "HanoCrux",
+					"link": "https://www.youtube.com/watch?v=u-KJ8T6r7EY",
+					"percent": 100,
+					"hz": "60hz"
+				},
+				{
+					"user": "St3am",
+					"link": "https://www.youtube.com/watch?v=Ns6H_lkKIl0",
+					"percent": 100,
+					"hz": "60hz"
+				},
+				{
+					"user": "hug",
+					"link": "https://www.youtube.com/watch?v=IzELzYcJ8hg",
+					"percent": 100,
+					"hz": "60hz"
+				},
+				{
+					"user": "Cukik",
+					"link": "https://www.youtube.com/watch?v=_cJ535CKxhA",
+					"percent": 100,
+					"hz": "300hz"
+				},
+
+			],
+			"name": "Dry Out v2",
+			"author": "Entropy",
+			"more": "none",
+			"id": 72163898,
+			"pass": "None",
+			"percentToQualify": 50,
+			"verificationVid": "https://www.youtube.com/watch?v=sIX75VhQlU4",
+			/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 16
+
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				// {
+				// 	"user": "",
+				// 	"link": "",
+				// 	"percent": 0,
+				// 	"hz": ""
+				// },
+
+			],
+			"name": "Idolize",
+			"author": "Screedican",
+			"more": "none",
+			"id": 73019566,
+			"pass": "None",
+			"percentToQualify": 50,
+			"verificationVid": "https://www.youtube.com/watch?v=n3HgMf3-kJ0",
+			/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 17
+
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				// {
+				// 	"user": "",
+				// 	"link": "",
+				// 	"percent": 0,
+				// 	"hz": ""
+				// },
+
+			],
+			"name": "New Mexcio",
+			"author": "Entropy",
+			"more": "none",
+			"id": 72114341,
+			"pass": "None",
+			"percentToQualify": 50,
+			"verificationVid": "https://www.youtube.com/watch?v=PWMW4G73XyQ",
+			/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 18
+
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				// {
+				// 	"user": "",
+				// 	"link": "",
+				// 	"percent": 0,
+				// 	"hz": ""
+				// },
+
+			],
+			"name": "Greasy Ass I",
+			"author": "Entropy",
+			"more": "none",
+			"id": 64951847,
+			"pass": "None",
+			"percentToQualify": 50,
+			"verificationVid": "https://www.youtube.com/watch?v=rFIKO4QMBMg",
+			/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 19
+
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				// {
+				// 	"user": "",
+				// 	"link": "",
+				// 	"percent": 0,
+				// 	"hz": ""
+				// },
+
+			],
+			"name": "shart",
+			"author": "KontrollFreek",
+			"more": "none",
+			"id": 76112871,
+			"pass": "None",
+			"percentToQualify": 100,
+			"verificationVid": "https://youtu.be/PYmS4nzdcgA",
+			/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+			"key": 21
+
+		},
+		/*=================================================================================*/
+		/*only modify version with every MAJOR update (you can always check update number at https://github.com/electroflameofficial/gdshittylist/releases*/
+
+	],
+	"version": [
+		1,
+		0,
+		0
+	]
+};
+
+const list = d.list; const version = d.version;
