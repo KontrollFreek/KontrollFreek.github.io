e508737659826950acd1e4c60b74d88ee01c47f8
Kontroll <72424960+KontrollFreek@users.noreply.github.com>
2022-03-31 18:27:00 -0400

    Delete discordgen directory

--- discordgen/img.js
+++
@@ -1,5 +0,0 @@
-$('#pfp').attr('src', 'pfp/purobook.png')
-$('#pfp').css('transform', 'translatex(16px) translatey(71px)')
-
-$('#col').css('background-image', 'url(banner/purobook.png)')
-$('#col').css('height', '120px')
