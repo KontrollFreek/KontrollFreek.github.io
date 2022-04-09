7406729ee2842e27dbe5c1f0b15b0793a26ce961
Kontroll <72424960+KontrollFreek@users.noreply.github.com>
2022-04-08 21:54:08 -0400

    Add files via upload

--- diffcord/load.js
+++ diffcord/load.js
@@ -1,16 +1,16 @@
 $(window).on('load', function() {
     $('#loadscreen').fadeOut(500)
-    $('#loadprog').attr('value', 'max')
-    console.log(loaded + ' loaded elements counted')
+    // $('#loadprog').attr('value', $('#loadprog').attr('max') )
+    // console.log(loaded + ' loaded elements counted')
 })
 
 $('#img').on('load', function() {
     $(this).css('background-color', '#00000000')
 })
 
-var loaded = 0
+// var loaded = 0
 
-$('*').on('load', function() {
-    loaded++
-    $('#loadprog').attr('value', loaded)
-})
+// $('*').on('load', function() {
+//     loaded++
+//     $('#loadprog').attr('value', loaded)
+// })
--- diffcord/tab.js
+++ diffcord/tab.js
@@ -1,3 +1,9 @@
-$('#navbar').children().click(function() {
-    console.log('clicked ' + $(this).attr('fortab'))
+$('#sidebar div').hide()
+$('#sidebar #1').show()
+$('#navbar button').click(function() {
+    $('#sidebar div').hide()
+    $('#navbar *').removeClass('hover')
+    $('#sidebar #' + $(this).attr('id')).show()
+    $('#navbar #' + $(this).attr('id')).addClass('hover')
+    $('#navbar #' + $(this).attr('id') + ' #svg').addClass('hover')
 })
