7a98c770f3ae1c95fa46ac51ecca1edfc3f19c1f
Kontroll <72424960+KontrollFreek@users.noreply.github.com>
2022-04-09 22:17:17 -0400

    Add files via upload

--- diffcord/load.js
+++ diffcord/load.js
@@ -1,11 +1,18 @@
-$(window).on('load', function() {
+$(window).on('load', function () {
     $('#loadscreen').fadeOut(500)
+    $.cookie('new-user', 'false') // temporary while i fix stuff
     // $('#loadprog').attr('value', $('#loadprog').attr('max') )
     // console.log(loaded + ' loaded elements counted')
-})
-
-$('#img').on('load', function() {
-    $(this).css('background-color', '#00000000')
+    $("#newusercenter").css("display", "none")
+    $("#newuser").css("transform", "scale(0)")
+    setTimeout(function () {
+        if ($.cookie('new-user') == undefined) {
+            $("#newusercenter").css("display", "flex")
+            $("#newuser").css('animation', 'fade 300ms cubic-bezier(0.000, 1.650, 1.000, 1.000)')
+            $("#newuser").css("transform", "scale(1)")
+            $.cookie('new-user', 'false')
+        }
+    }, 500)
 })
 
 // var loaded = 0
