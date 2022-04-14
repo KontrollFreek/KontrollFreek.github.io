fca3c5559901556ebf3a17b2af012c582c85dc8c
Kontroll <72424960+KontrollFreek@users.noreply.github.com>
2022-04-13 20:53:56 -0400

    Delete diffcord directory

--- diffcord/js/tab.js
+++
@@ -1,9 +0,0 @@
-$('#sidebar div').hide()
-$('#sidebar #1').show()
-$('#navbar button').click(function() {
-    $('#sidebar div').hide()
-    $('#navbar *').removeClass('hover')
-    $('#sidebar #' + $(this).attr('id')).show()
-    $('#navbar #' + $(this).attr('id')).addClass('hover')
-    $('#navbar #' + $(this).attr('id') + ' #svg').addClass('hover')
-})
